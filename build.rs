5f7b4ac8ff90be4f92c4318a1cb5558db8ed7968
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-10-27 02:40:50 +0530

    fix: proto build added

--- build.rs
+++ build.rs
@@ -1,16 +1,23 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let network_manager = "./proto/network_manager.proto";
-    // tonic_build::configure()
-    //     .build_client(true)
-    //     .compile(&[network_manager], &["./proto/network_manager.proto"])?;
-    // Ok(())
+    let display_manager = "./proto/display_manager.proto";
+    let device_metrics = "./proto/metrics_manager.proto";
+    let cpu_governor_ctrl = "./proto/cpu_governor_ctrl.proto";
+    let device_info = "./proto/device_info.proto";
 
     tonic_build::configure()
         .build_server(true)
-        // .out_dir("src/")
-        // adding attributes
         .type_attribute(".", "#[derive(serde::Deserialize, serde::Serialize,)]")
-        .compile(&[network_manager], &["."])
+        .compile(
+            &[
+                network_manager,
+                display_manager,
+                device_metrics,
+                cpu_governor_ctrl,
+                device_info
+            ],
+            &["."],
+        )
         .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
 
     Ok(())
